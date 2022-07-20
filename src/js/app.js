24ff880af99cc4e24f2a0fbb32d61c39b3db3577
Annie Bauer <overheads165@gmail.com>
2022-07-20 14:44:25 +0300

    tests

--- src/js/app.js
+++ src/js/app.js
@@ -1,17 +1,26 @@
 class Validator {
   validateUsername = (name) => {
-    // имя содержит только латинские буквы,цифры от 0 до 9, - и _
-    if (name.match(/[^A-Za-z0-9-_]/) != -1) {
+    // имя содержит только латинские буквы, цифры от 0 до 9, - и _
+    if (name.match(/[^A-Za-z0-9-_]/) != null) {
       return false;
     }
+
     // имя не начинается и не заканчивается цифрами
-    if (name.match(/^[^\d]+$/) == -1) {
+    if (name.match(/^[^\d]+$/) == null) {
       return false;
     }
+
     // имя не имеет три цифры подряд в любом месте
-    if (name.match(/\d{3,}/) == -1) {
+    if (name.match(/\d{3,}/) != null) {
       return false;
     }
+
     return true;
   };
 }
+
+export default Validator;
+
+//const val = new Validator()
+//const input = ["Пятачок", "Piglet", "Piglet12", "12Pig", "P_123_G", "-Piglet-", "_pigLet_", "Pig-Ж-let"]
+//input.map(val.validateUsername)
---
+++ src/js/app.test.js
@@ -0,0 +1,38 @@
+import Validator from "./app";
+
+const val = new Validator();
+
+test("функция не пропускает имена которые содержат что-то кроме латинских буквы, цифр от 0 до 9, - и _", () => {
+  const input = "Пяточок";
+  const received = val.validateUsername(input);
+  const expected = false;
+  expect(received).toBe(expected);
+});
+
+test("функция пропускает имена которые содержат только латинские буквы, цифры от 0 до 9, - и _", () => {
+  const input = "Piglet";
+  const received = val.validateUsername(input);
+  const expected = true;
+  expect(received).toBe(expected);
+});
+
+test("имена которые не начинаются цифрами", () => {
+  const input = "123Pop";
+  const received = val.validateUsername(input);
+  const expected = false;
+  expect(received).toBe(expected);
+});
+
+test("имена которые не заканчиваются цифрами", () => {
+  const input = "Pop123";
+  const received = val.validateUsername(input);
+  const expected = false;
+  expect(received).toBe(expected);
+});
+
+test("функция выводит имена, которые не имеют три цифры подряд в любом месте", () => {
+  const input = "O456pop";
+  const received = val.validateUsername(input);
+  const expected = false;
+  expect(received).toEqual(expected);
+});
